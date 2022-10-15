4d2e98d0f40fb854f53a879f43365355ece6330d
DevLeoSales <leonardo.t.sales94@gmail.com>
2022-10-15 08:47:17 -0300

    Desafio de aniversário feito

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <main>
+    <section className="container">
+      <List />
+    </section>
+  </main>;
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import data from './data';
+
 
 const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+  const [people, setPeople] = React.useState(data)
+  const qtdeAniversariosHoje = people.length
+  return <>
+    <h2>{qtdeAniversariosHoje} aniversários hoje</h2>
+    {
+      people.map((person) => {
+        const { id, name, age, image } = person
+        return (
+          <div key={id} className='item' >
+            <section className='person'>
+              <img src={image} />
+              <section>
+                <h4>{name}</h4>
+                <p>{age} anos</p>
+              </section>
+            </section>
+          </div>
+        )
+      })
+    }
+    <button className='btn' onClick={() => setPeople([])}>Limpar tudo</button>
+  </>;
 };
 
 export default List;
