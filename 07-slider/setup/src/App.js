460221de066c9cbcbd61a07d458498b8da1da782
DevLeoSales <leonardo.t.sales94@gmail.com>
2022-10-20 09:00:16 -0300

    desafio slider

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,7 +3,58 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const [index, setIndex] = useState(0)
+  const { id, image, name, title, quote } = data[index]
+
+  const checkNumber = (number) => {
+    if (number > data.length - 1) {
+      return 0
+    }
+    if (number < 0) {
+      return data.length - 1
+    }
+    return number
+  }
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1
+      return checkNumber(newIndex)
+    })
+  }
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1
+      return checkNumber(newIndex)
+    })
+  }
+
+
+  // setTimeout(() => {
+  //   nextPerson()
+  // }, 3000);
+
+  return <>
+    <div className="title">
+      <h2><span>/</span> Reviews</h2>
+    </div>
+    <article className="section-center">
+      <button className="prev" onClick={prevPerson}>
+        <FiChevronLeft />
+      </button>
+      <img src={image} alt={name} className="person-img" />
+      <h4>{name}</h4>
+      <p className='title'>{title}</p>
+      <p className="text">{quote}</p>
+      <button className="next" onClick={nextPerson}>
+        <FiChevronRight />
+      </button>
+      <span className="icon">
+        <FaQuoteRight />
+      </span>
+    </article>
+  </>;
 }
 
 export default App;
