92cef522adb52826ae1604480a649b8b7193d839
DevLeoSales <leonardo.t.sales94@gmail.com>
2022-10-25 12:08:08 -0300

    desafio grocery bud

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,40 +2,93 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if (list) {
+    return JSON.parse(localStorage.getItem('list'))
+  } else {
+    return []
+  }
+}
+
 function App() {
-  const [item, setItem] = useState('')
-  const [groceryList, setGroceryList] = useState([])
-  const [alert, setAlert] = useState(false)
+  const [name, setName] = useState('')
+  const [groceryList, setGroceryList] = useState(getLocalStorage())
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: ''
+  })
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
 
   useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(groceryList))
+  }, [groceryList])
 
-  }, [groceryList]);
+  const clearAllItems = () => {
+    showAlert(true, 'danger', 'list deleted')
+    setGroceryList([])
+  }
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg })
+  }
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed')
+    setGroceryList(groceryList.filter((item) => item.id !== id))
+  }
+
+  const editItem = (id) => {
+    const currentItem = groceryList.find((item) => item.id === id)
+    setIsEditing(true)
+    setEditID(id)
+    setName(currentItem.title)
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setGroceryList([...groceryList, item])
-    setItem('')
-    // console.log(groceryList);
+    if (!name) {
+      showAlert(true, 'danger', 'please enter value')
+    } else if (name && isEditing) {
+      setGroceryList(
+        groceryList.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name }
+          }
+          return item
+        }))
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+      showAlert(true, 'success', 'value changed')
+    } else {
+      showAlert(true, 'success', 'item added to the list')
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setGroceryList([...groceryList, newItem])
+      setName('')
+    }
   }
 
-  const handleChange = (e) => {
-    const item = e.target.value
-    setItem(item)
-    // console.log(item);
-  }
-
-  return <>
-    <section className='section-center' >
-      <section className='grocery-form'>
-        <h3>Grocery Bud</h3>
-      </section>
-      <form className="form-control" onSubmit={handleSubmit}>
-        <input type="text" className='grocery' value={item} onChange={handleChange} />
-        <button className="submit-btn" type='submit' >Submit</button>
-      </form>
-      <List groceryList={groceryList} />
+  return <section className='section-center' >
+    <section className='grocery-form'>
+      {alert.show && <Alert {...alert} removeAlert={showAlert} list={groceryList} />}
+      <h3>Grocery Bud</h3>
     </section>
-  </>
+    <form className="form-control" onSubmit={handleSubmit}>
+      <input placeholder='e.g. eggs' type="text" className='grocery' value={name} onChange={(e) => setName(e.target.value)} />
+      <button className="submit-btn" type='submit'>
+        {isEditing ? 'edit' : 'submit'}
+      </button>
+    </form>
+    {groceryList.length > 0 && (
+      <div className='grocery-container'>
+        <List groceryList={groceryList} removeItem={removeItem} editItem={editItem} />
+        <button className="clear-btn" onClick={clearAllItems}>clear all</button>
+      </div>
+    )}
+  </section>
 }
 
 export default App
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 
-const List = ({ groceryList }) => {
+const List = ({ groceryList, editItem, removeItem }) => {
   return <>
-    <section className="grocery-container">
-      {groceryList.map((item, index) => {
-        return <>
-          <p key={index}>{item}</p>
-          <button className="edit-btn"><FaEdit /></button>
-          <button className="delete-btn"><FaTrash /></button>
-        </>
+    <section className="grocery-list">
+      {groceryList.map((item) => {
+        const { id, title } = item
+        return <article key={id} className='grocery-item'>
+          <p className='title'>{title}</p>
+          <div className="btn-container">
+            <button type='button' className="edit-btn" onClick={() => editItem(id)}><FaEdit /></button>
+            <button type='button' className="delete-btn" onClick={() => removeItem(id)}><FaTrash /></button>
+          </div>
+        </article>
       })}
-
-      <button className="clear btn">clear all</button>
     </section>
   </>
 }
