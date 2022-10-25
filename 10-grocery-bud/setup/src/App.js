36d5d186f201f557d40d671b9840bff4b5d56060
DevLeoSales <leonardo.t.sales94@gmail.com>
2022-10-24 22:35:48 -0300

    commit inicial - desafio grocery bud

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,39 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [item, setItem] = useState('')
+  const [groceryList, setGroceryList] = useState([])
+  const [alert, setAlert] = useState(false)
+
+  useEffect(() => {
+
+  }, [groceryList]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setGroceryList([...groceryList, item])
+    setItem('')
+    // console.log(groceryList);
+  }
+
+  const handleChange = (e) => {
+    const item = e.target.value
+    setItem(item)
+    // console.log(item);
+  }
+
+  return <>
+    <section className='section-center' >
+      <section className='grocery-form'>
+        <h3>Grocery Bud</h3>
+      </section>
+      <form className="form-control" onSubmit={handleSubmit}>
+        <input type="text" className='grocery' value={item} onChange={handleChange} />
+        <button className="submit-btn" type='submit' >Submit</button>
+      </form>
+      <List groceryList={groceryList} />
+    </section>
+  </>
 }
 
 export default App
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+
+const List = ({ groceryList }) => {
+  return <>
+    <section className="grocery-container">
+      {groceryList.map((item, index) => {
+        return <>
+          <p key={index}>{item}</p>
+          <button className="edit-btn"><FaEdit /></button>
+          <button className="delete-btn"><FaTrash /></button>
+        </>
+      })}
+
+      <button className="clear btn">clear all</button>
+    </section>
+  </>
 }
 
 export default List
