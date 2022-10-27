55fb99a5769ad95a1f1190994a3bf4283d196e8d
DevLeoSales <leonardo.t.sales94@gmail.com>
2022-10-27 19:25:02 -0300

    desafio navbar-versão final (com ajuste na rotação do menu hamburger)

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,3 +1,4 @@
+import userEvent from '@testing-library/user-event'
 import React, { useState, useRef, useEffect } from 'react'
 import { FaBars, FaTwitter, FaLinkedin } from 'react-icons/fa'
 import { links, social } from './data'
@@ -5,17 +6,38 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false)
+  // const [ishandleClicked, setIsMenuClicked] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+  const menuClick = useRef(null)
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    console.log(linksContainerRef.current.getBoundingClientRect());
+
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+      menuClick.current.style.transform = "rotate(90deg)"
+    } else {
+      linksContainerRef.current.style.height = '0px'
+      menuClick.current.style.transform = "rotate(0deg)"
+    }
+  }, [showLinks])
+  const handleClick = (e) => {
+    setShowLinks(!showLinks)
+    console.log(e.target.value)
+  }
   return <>
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
+          <button className='nav-toggle' onClick={handleClick} ref={menuClick}>
             <FaBars />
           </button>
         </div>
-        <div className={`${showLinks ? "links-container show-container" : "links-container"}`} >
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link
               return <li key={id}>
