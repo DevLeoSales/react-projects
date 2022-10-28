ced0252233ec40c2d28bcbabc61b797853a378df
DevLeoSales <leonardo.t.sales94@gmail.com>
2022-10-28 14:12:54 -0300

    desafio modal/sidebar

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,15 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { AppContext, useGlobalContext } from './context'
 
 const Home = () => {
-  return <h2>home component</h2>
+  const { openModal, openSideBar } = useGlobalContext()
+  return <main>
+    <button className="sidebar-toggle" onClick={openSideBar}>
+      <FaBars />
+    </button>
+    <button className="btn" onClick={openModal}>show modal</button>
+  </main>
 }
 
 export default Home
